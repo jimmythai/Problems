815e9b5bad8175040ee5bd1d0d48083e6d16d64d
AUTHOR_NAME <AUTHOR_EMAIL>
2022-09-09 18:24:24 +0700

    Initial Commit

---
+++ Problems/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Problems
+//
+//  Created by Atsushi Yamamoto on 2022/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Problems/ProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProblemsApp.swift
+//  Problems
+//
+//  Created by Atsushi Yamamoto on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct ProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
